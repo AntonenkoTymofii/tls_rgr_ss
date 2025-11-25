5a28a14305f9c1a278a9977ecb9ed983ba69ddf6
AntonenkoTymofii <antonenkotimofii@gmail.com>
2025-11-25 12:33:58 +0200

    implement handshake logic

--- main.py
+++ main.py
@@ -120,3 +120,53 @@ class Client:
         ciphertext = encrypted_data[12:]
         aesgcm = AESGCM(self.session_key)
         return aesgcm.decrypt(nonce, ciphertext, None)
+
+
+def run_tls_simulation():
+    print("=== ПОЧАТОК СИМУЛЯЦІЇ TLS HANDSHAKE ===\n")
+
+    network = SimulatedNetwork(packet_size_limit=50, delay=0.05)
+    client = Client()
+    server = Server()
+
+    client.client_random = os.urandom(32)
+    msg_client_hello = b"ClientHello" + client.client_random
+    received_hello = network.send("Client", msg_client_hello, "Client Hello + Random")
+
+    server.server_random = os.urandom(32)
+    server_pub_key_bytes = server.get_public_key_bytes()
+    msg_server_hello = b"ServerHello" + server.server_random + server_pub_key_bytes
+    received_server_resp = network.send("Server", msg_server_hello, "Server Hello + Key")
+
+    server_rand_from_msg = received_server_resp[11:43]
+    pub_key_from_msg = received_server_resp[43:]
+
+    premaster_secret = os.urandom(32)
+    encrypted_premaster = client.encrypt_premaster(pub_key_from_msg, premaster_secret)
+    rec_enc_premaster = network.send("Client", encrypted_premaster, "Encrypted Premaster")
+
+    decrypted_premaster = server.process_premaster(rec_enc_premaster)
+
+    print("\n--- Генерація сеансових ключів ---")
+    client.generate_session_key(premaster_secret, server_rand_from_msg)
+    server.generate_session_key(decrypted_premaster, client.client_random)
+
+    client_ready_msg = client.encrypt_message(b"Client Ready")
+    rec_client_ready = network.send("Client", client_ready_msg, "Encrypted 'Client Ready'")
+    server.decrypt_message(rec_client_ready)
+
+    server_ready_msg = server.encrypt_message(b"Server Ready")
+    rec_server_ready = network.send("Server", server_ready_msg, "Encrypted 'Server Ready'")
+    client.decrypt_message(rec_server_ready)
+
+    print("\n=== HANDSHAKE ЗАВЕРШЕНО. ПЕРЕДАЧА ДАНИХ ===\n")
+    secret_message = "Це секретне повідомлення для РГР. Шифрування AES-GCM.".encode('utf-8')
+    encrypted_chat_msg = client.encrypt_message(secret_message)
+    rec_chat_msg = network.send("Client", encrypted_chat_msg, "DATA MESSAGE")
+
+    decrypted_chat_text = server.decrypt_message(rec_chat_msg).decode('utf-8')
+    print(f"\nServer Received: {decrypted_chat_text}")
+
+
+if __name__ == "__main__":
+    run_tls_simulation()
